cd66c8541cdc3eff64920b74ebfc60e47e26d29f
Tomas Colloca <tomascllc94@gmail.com>
2017-11-11 15:48:15 -0300

    drop tables fixed (?

--- src/CallMemberGenerator.java
+++ src/CallMemberGenerator.java
@@ -62,12 +62,9 @@ public class CallMemberGenerator {
 	}
 	
 	private static void dropTables(Connection conn, int schema) throws SQLException {
-		conn.createStatement().execute("IF EXISTS(SELECT * FROM sysobjects WHERE name='grupo2v" + schema + ".Call') "
-				+ "DROP TABLE grupo2v" + schema + ".Call");
-		conn.createStatement().execute("IF EXISTS(SELECT * FROM sysobjects WHERE name='grupo2v" + schema + ".EmisorReceptor') "
-				+ "DROP TABLE grupo2v" + schema + ".EmisorReceptor");
-		conn.createStatement().execute("IF EXISTS(SELECT * FROM sysobjects WHERE name='grupo2v" + schema + ".DateTime') "
-				+ "DROP TABLE grupo2v" + schema + ".DateTime");
+		conn.createStatement().execute("DROP TABLE IF EXISTS grupo2v" + schema + ".Call");
+		conn.createStatement().execute("DROP TABLE IF EXISTS grupo2v" + schema + ".EmisorReceptor");
+		conn.createStatement().execute("DROP TABLE IF EXISTS grupo2v" + schema + ".DateTime");
 		conn.commit();
 	}
 	
